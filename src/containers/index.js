6d3349d6f311dfbf425012801f3a006a896c76b7
Raymond Xu <rxu@zendesk.com>
2018-12-05 08:39:19 +0800

    Refactor structure

--- src/containers/index.js
+++ src/containers/index.js
@@ -1,2 +1,2 @@
-export { default as PhotoCreateForm } from './PhotoCreateForm/PhotoCreateForm';
-export { default as PhotoList } from './PhotoList/PhotoList';
+export { default as PhotoCreateForm } from './PhotoCreateForm';
+export { default as PhotoList } from './PhotoList';
