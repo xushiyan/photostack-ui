5f3bc4275a08b18b4730015a2565eb1ce48a7d95
Raymond Xu <rxu@zendesk.com>
2018-12-05 13:14:10 +0800

    Add redux store to <App />

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import promise from 'redux-promise';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import Amplify from 'aws-amplify';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import reducers from './reducers';
 
 Amplify.configure({
   API: {
@@ -18,7 +23,14 @@ Amplify.configure({
   },
 });
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(reducers, composeWithDevTools(applyMiddleware(promise)));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
